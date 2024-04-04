0020f8cda3748b6fa2b8ba074d09eb17f8cfc71c
Olumayowa Kayode <olumayowa@Olumayowa-MacBook-Pro.local>
2024-04-04 19:38:18 +0100

    fix deploy error

--- src/Components/HomePage/LandingPage/LandingPage.jsx
+++ src/Components/HomePage/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import Card from '../Card/Card'
-import Card2 from '../card2/Card2'
+import CardTwo from '../Card2/CardTwo'
 import ScrollToTopOnMount from '../../ScrollToTopOnMount/ScrollToTopOnMount'
 
 const LandingPage = () => {
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <ScrollToTopOnMount />
     <Hero />
     <Card />
-    <Card2 />
+    <CardTwo />
     </>
   )
 }
